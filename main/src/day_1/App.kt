4ec3b6f3948c1468dd9685e11ee9a79341c553f6
Don Vicente <m.vicent@gmail.com>
2021-12-06 02:36:31 -0300

    fixes

--- main/src/day_1/App.kt
+++ main/src/day_1/App.kt
@@ -19,14 +19,13 @@ fun main() {
     var previous = 0
     measurementList.forEachIndexed { index, item ->
         val subSet = measurementList.subList(index, measurementList.lastIndex+1).take(3)
-        val calculation = subSet.fold(0) { a, c -> a+c }
+        val calculation = subSet.reduce { a, c -> a+c }
         if (subSet.size == 3) {
             if (previous < calculation) {
                 result2 += 1
             }
             previous = calculation
         }
-
     }
     println(result2)
 }
