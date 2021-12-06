53bf009afd873b49c4722c470b9369b3297ce9df
Don Vicente <m.vicent@gmail.com>
2021-12-06 02:34:39 -0300

    part 2

--- main/src/day_1/App.kt
+++ main/src/day_1/App.kt
@@ -3,12 +3,30 @@ package day_1
 import FileHelper
 
 fun main() {
+    //--- Part One ---
+    val measurementList = FileHelper.loadInput("day_1/input.txt")
     var result = -1
-    FileHelper.loadInput("day_1/input.txt").fold(0) { a, c ->
+    measurementList.fold(0) { a, c ->
         if (a < c) {
             result += 1
         }
         c
     }
-    print(result)
+    println(result)
+
+    //--- Part Two ---
+    var result2 = -1
+    var previous = 0
+    measurementList.forEachIndexed { index, item ->
+        val subSet = measurementList.subList(index, measurementList.lastIndex+1).take(3)
+        val calculation = subSet.fold(0) { a, c -> a+c }
+        if (subSet.size == 3) {
+            if (previous < calculation) {
+                result2 += 1
+            }
+            previous = calculation
+        }
+
+    }
+    println(result2)
 }
